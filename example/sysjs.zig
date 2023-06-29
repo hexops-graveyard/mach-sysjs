d25c5b8c808519dff9724cfb17ab5312a7482699
Stephen Gutekanst <stephen@hexops.com>
2023-06-28 18:36:53 -0700

    begin migrating to zig AST parsing strategy
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- example/sysjs.zig
+++ example/sysjs.zig
@@ -1,3 +1,16 @@
+//! A file-level comment
+
+const builtin = @import("builtin");
+
 pub const console = struct {
-    pub const log = fn (string: []const u8) void;
+    /// console.log invokes the JS console.log API
+    log: fn (string: []const u8) void,
+
+    log2: fn (string: []const u8, string2: []const u8) void,
 };
+
+/// doPrint does stuff
+pub fn doPrint() void {
+    // use console.log
+    console.log("zig:js.console.log(\"hello from Zig\")");
+}
