d855978f1e405b4f8f7c24835af960f34f9c4b53
iddev5 <ayushbardhan5@gmail.com>
2023-07-09 16:28:49 +0530

    Implement proper generation of zig types in their extern form
    Types like slices will now be generated as pair of its pointer and its
    length. This behavior is implemented for both arguments (function calls)
    as well as parameters (function declarations).

--- example/sysjs_generated.zig
+++ example/sysjs_generated.zig
@@ -3,15 +3,15 @@
 const builtin = @import("builtin");
 
 pub const console = struct {
-    extern fn sysjs_console_log(string: []const u8) void;
-    extern fn sysjs_console_log2(string: []const u8, []const u8) void;
+    extern fn sysjs_console_log(string: [*]const u8, string_len: u32) void;
+    extern fn sysjs_console_log2(string: [*]const u8, string_len: u32, [*]const u8, u32) void;
 
     pub inline fn log(string: []const u8) void {
-        return sysjs_console_log(string);
+        return sysjs_console_log(string.ptr, string.len);
     }
 
     pub inline fn log2(string: []const u8, v1: []const u8) void {
-        return sysjs_console_log2(string, v1);
+        return sysjs_console_log2(string.ptr, string.len, v1.ptr, v1.len);
     }
 };
 
